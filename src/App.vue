9368229f8923d3a8ff94349efc6656beb83afb8b
Mowalky <dev@mowalky.com>
2021-10-29 18:12:26 -0500

    added tailwind css

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,47 @@ function reset() {
 
 <template>
   <div>
-    <Count :count="count" />
-    <button @click="reset">Reset</button>
+    <div
+      class="
+        p-6
+        max-w-sm
+        mx-auto
+        bg-white
+        rounded-xl
+        shadow-md
+        flex
+        items-center
+        space-x-4
+      "
+    >
+      <div class="flex-shrink-0">
+        <img
+          class="h-12 w-12"
+          @dblclick="reset"
+          src="https://vitejs.dev/logo.svg"
+        />
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">Vite & Tailwind</div>
+        <p class="text-gray-500">You have a {{ count }} message!</p>
+      </div>
+    </div>
+
+    <button
+      @click="reset"
+      class="
+        mt-5
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded
+      "
+    >
+      Reset
+    </button>
   </div>
 </template>
 
